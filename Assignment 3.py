fa1fee498f7c90e426b8b888acee2c9f294dc0af
paliwaldeepak <114053130+paliwaldeepak@users.noreply.github.com>
2022-11-01 16:37:23 +0530

    Add files via upload

---
+++ Assignment 3.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #1. Why are functions advantageous to have in your programs?
+
+# ans:- Using function it makes programs shorter, easier to read, and easier to update and also  reduce the need for duplicate code.
+
+# #2 When does the code in a function run: when it's specified or when it's called?
+
+# ans:- The code in a function run when the function is called, not when the function is defined.
+
+# #3. What statement creates a function?
+
+# ans:- The def statement create a function.
+
+# #4. What is the difference between a function and a function call?
+
+# ans:- A function consists of the def statement and the code in its def clause.
+# 
+# A function call is what moves the program execution into the function, and the function call evaluates to the function's return value.
+
+# #5. How many global scopes are there in a Python program? How many local scopes?
+
+# ans:- There is one global scope, and a local scope is created whenever a function is called.
+
+# #6. What happens to variables in a local scope when the function call returns?
+
+# ans:-When a function returns, the local scope is destroyed, and all the variables in it are forgotten.
+
+# #7. What is the concept of a return value? Is it possible to have a return value in an expression?
+
+# ans:- A return value is the value that a function call evaluates to. Like any value, a return value can be used as part of an expression.
+
+# #8. If a function does not have a return statement, what is the return value of a call to that function?
+
+# ans:- If there is no return statement for a function, its return value is None
+
+# #9. How do you make a function variable refer to the global variable?
+
+# ans:-A global statement will force a variable in a function to refer to the global variable.
+
+# #10. What is the data type of None?
+
+# ans:- The data type of None is NoneType.
+
+# #11. What does the sentence import areallyourpetsnamederic do?
+
+# ans:- That import statement imports a module named areallyourpetsnamederic
+
+# #12. If you had a bacon() feature in a spam module, what would you call it after importing spam?
+
+# ans:- This function can be called with spam.bacon().
+
+# #13. What can you do to save a programme from crashing if it encounters an error?
+
+# ans:- Place the line of code that might cause an error in a try clause.
+
+# #14. What is the purpose of the try clause? What is the purpose of the except clause?
+
+# ans:- he purpose of the try clause to save a programme from crashing if it encounters an error.
+# 
+# the purpose of the except clause that executes if an error happens goes in the except clause.
